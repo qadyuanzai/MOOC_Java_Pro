a6195b042755948c8efeed7a8074f7d7c935e527
qadyuanzai <42380987+qadyuanzai@users.noreply.github.com>
2019-05-14 22:56:41 +0800

    2019/05/14 22:56

---
+++ dome/src/dome/CD.java
@@ -0,0 +1,27 @@
+package dome;
+
+public class CD {
+	private String title;
+	private String artist;
+	private int numofTracks;
+	private int playingTimes;
+	private boolean gotIt = false;
+	private String comment;
+	
+	
+	public CD(String title, String artist, int numofTracks, int playingTimes, String comment) {
+//		super();
+		this.title = title;
+		this.artist = artist;
+		this.numofTracks = numofTracks;
+		this.playingTimes = playingTimes;
+		this.comment = comment;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
